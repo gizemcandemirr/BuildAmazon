2565754685d27625dd35ba025b1d4018d6d84d2b
Gizem <gizem.candemir@secilstore.com>
2022-04-11 12:45:39 +0300

    Building the Banner Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-gray-100">
       <Head>
         <title>Create Amazon</title>
       </Head> 
 
        {/* header */}          
        <Header />
-       <main>
+       <main className='max-w-screen-2xl mx-auto'>
          {/* banner */}
+         <Banner />
          
          {/* products */}
        </main>
