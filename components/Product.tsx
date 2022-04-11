543f12fd439b9a85d47eec22ea83a8fdb344a8b3
Gizem <gizem.candemir@secilstore.com>
2022-04-11 16:13:11 +0300

    convert to typescript

--- components/Product.tsx
+++ components/Product.tsx
@@ -3,10 +3,20 @@ import React, { useState } from 'react'
 import {StarIcon} from "@heroicons/react/solid"
 import Currency from 'react-currency-format';
 
+type Props = {
+    id: number;
+    title:string;
+    price: number;
+    description:string;
+    category:string;
+    image: string;
+    
+}
 const MAX_RATING =5;
 const MIN_RATING =1;
 
-function Product({id, title, price, description, category, image}) {
+
+function Product(props: Props) {
    
     const [rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
@@ -14,10 +24,10 @@ function Product({id, title, price, description, category, image}) {
     const [hasPrime] = useState(Math.random() < 0.5)
 
   return (
-    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-        <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
-        <Image src={image} width={200} height={200} objectFit="contain" />
-        <h4 className='my-3'>{title}</h4>
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10' key={props.id}>
+        <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{props.category}</p>
+        <Image src={props.image} width={200} height={200} objectFit="contain" />
+        <h4 className='my-3'>{props.title}</h4>
 
         <div className='flex'>
             {
@@ -26,10 +36,10 @@ function Product({id, title, price, description, category, image}) {
                 ))
             }
         </div>
-      <p className='text-xs my-2 line-clamp-2'>{ description } </p>  
+      <p className='text-xs my-2 line-clamp-2'>{ props.description } </p>  
       <div className='mb-5'>
           <Currency
-            value={price}
+            value={props.price}
             prefix={'$'}
           />
           {hasPrime && (
