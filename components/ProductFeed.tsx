a1ebe7dbaa4e4b4f689894e06b467c128937336a
Gizem <gizem.candemir@secilstore.com>
2022-04-11 15:25:28 +0300

    Building the Product Component

--- components/ProductFeed.tsx
+++ components/ProductFeed.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Product from './Product'
 
 
 
@@ -6,7 +7,16 @@ function ProductFeed({products}) {
   return (
     <div>
         {products.map(({id, title, price, description, category, image}) => (
-            <p> {title}</p>
+            
+            <Product 
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description= {description}
+            category= {category}
+            image= {image}
+            />
         ))}
     </div>
   )
